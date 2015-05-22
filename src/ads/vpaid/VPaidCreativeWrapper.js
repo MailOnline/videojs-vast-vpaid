481dedcb155698d2a4105f3df159ad138a98f8a6
Carlos <carpasse@gmail.com>
2015-05-22 11:42:13 +0200

    Finished creative wrapper

--- src/ads/vpaid/VPaidCreativeWrapper.js
+++ src/ads/vpaid/VPaidCreativeWrapper.js
@@ -1,19 +1,23 @@
-// This class is meant to be part of the video player that interacts with the Ad.
-// It takes the VPAID creative as a parameter in its contructor.
-function VPAIDCreativeWrapper(vpaidCreative) {
+function VPAIDCreativeWrapper(vpaidCreative, opts) {
   if (!(this instanceof VPAIDCreativeWrapper)) {
     return new VPAIDCreativeWrapper(vpaidCreative);
   }
-  sanityCheck(vpaidCreative);
+  sanityCheck(vpaidCreative, opts);
+  var defaultOpts = {
+    responseTimeout: 2000
+  };
 
+  this.options = extend({}, defaultOpts, opts || {});
   this._creative = vpaidCreative;
-  //this.setCallbacksForCreative();
-  // This function registers the callbacks of each of the events
 
   /*** Local Functions ***/
-  function sanityCheck(creative) {
+  function sanityCheck(creative, opts) {
     if (!creative || !VPAIDCreativeWrapper.checkVPAIDInterface(creative)) {
       throw new VASTError('on VPAIDCreativeWrapper, the passed VPAID creative does not fully implement the VPAID interface');
+    }
+
+    if (opts && !isObject(opts)) {
+      throw new VASTError("on VPAIDCreativeWrapper, expected options hash  but got '" + opts + "'");
     }
   }
 }
@@ -32,75 +36,164 @@ VPAIDCreativeWrapper.checkVPAIDInterface = function checkVPAIDInterface(VPAIDCre
   return true;
 };
 
-/*
- var events = [ 'AdStarted', 'AdStopped', 'AdSkipped', 'AdLoaded', 'AdLinearChange', 'AdSizeChange', 'AdExpandedChange', 'AdSkippableStateChange',
- 'AdDurationChange', 'AdRemainingTimeChange', 'AdVolumeChange', 'AdImpression', 'AdClickThru', 'AdInteraction', 'AdVideoStart',
- 'AdVideoFirstQuartile','AdVideoMidpoint', 'AdVideoThirdQuartile', 'AdVideoComplete', 'AdUserAcceptInvitation', 'AdUserMinimize',
- 'AdUserClose', 'AdPaused', 'AdPlaying', 'AdError', 'AdLog'];
- */
+VPAIDCreativeWrapper.prototype.creativeAsyncCall = function () {
+  var args = arrayLikeObjToArray(arguments);
+  var method = args.shift();
+  var cb = args.pop();
+  var that = this;
+  var timeoutId;
 
-VPAIDCreativeWrapper.prototype.on = function onEvent() {
+  sanityCheck(method, cb, this._creative);
+  args.push(wrapCallback());
 
+  this._creative[method].apply(this, args);
+  timeoutId = setTimeout(function () {
+    timeoutId = null;
+    cb(new VASTError("on VPAIDCreativeWrapper, timeout while waiting for a response on call '" + method + "'"));
+    that.destroy();
+    cb = noop;
+  }, this.options.responseTimeout);
+
+  /*** Local functions ***/
+  function sanityCheck(method, cb, creative) {
+    if (!isString(method) || !isFunction(creative[method])) {
+      throw new VASTError("on VPAIDCreativeWrapper.creativeAsyncCall, invalid method name")
+    }
+
+    if (!isFunction(cb)) {
+      throw new VASTError("on VPAIDCreativeWrapper.creativeAsyncCall, missing callback")
+    }
+  }
+
+  function wrapCallback() {
+    return function () {
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+      cb.apply(this, arguments);
+    };
+  }
 };
 
-VPAIDCreativeWrapper.prototype.initAd = function (width, height, viewMode, desiredBitrate, creativeData, environmentVars) {
-  this._creative.initAd(width, height, viewMode, desiredBitrate, creativeData, environmentVars);
+VPAIDCreativeWrapper.prototype.destroy = function () {
+  this._creative.unloadAdUnit();
 };
 
-VPAIDCreativeWrapper.prototype.startAd = function () {
-  console.log("startAd");
-  this._creative.startAd();
+VPAIDCreativeWrapper.prototype.on = function (evtName, handler) {
+  var addEventListener = this._creative.addEventListener || this._creative.subscribe || this._creative.on;
+  addEventListener.call(this._creative, evtName, handler);
 };
 
-VPAIDCreativeWrapper.prototype.stopAd = function () {
-  this._creative.stopAd();
+VPAIDCreativeWrapper.prototype.off = function (evtName, handler) {
+  var removeEventListener = this._creative.removeEventListener || this._creative.unsubscribe || this._creative.off;
+  removeEventListener.call(this._creative, evtName, handler);
 };
 
-VPAIDCreativeWrapper.prototype.resizeAd = function (width, height, viewMode) {
+VPAIDCreativeWrapper.prototype.waitForEvent = function (evtName, cb, context) {
+  var timeoutId;
+  sanityCheck(evtName, cb);
+  context = context || null;
+
+  this.on(evtName, responseListener);
+
+  timeoutId = setTimeout(function () {
+    cb(new VASTError("on VPAIDCreativeWrapper.waitForEvent, timeout while waiting for event '" + evtName + "'"));
+    timeoutId = null;
+    cb = noop;
+  }, this.options.responseTimeout);
+
+  /*** Local functions ***/
+  function sanityCheck(evtName, cb) {
+    if (!isString(evtName)) {
+      throw new VASTError("on VPAIDCreativeWrapper.waitForEvent, missing evt name")
+    }
+
+    if (!isFunction(cb)) {
+      throw new VASTError("on VPAIDCreativeWrapper.waitForEvent, missing callback")
+    }
+  }
+
+  function responseListener() {
+    var args = arrayLikeObjToArray(arguments);
+
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+      timeoutId = null;
+    }
+
+    args.unshift(null);
+    cb.apply(context, args);
+  }
+};
+
+// VPAID METHODS
+VPAIDCreativeWrapper.prototype.handshakeVersion = function (version, cb) {
+  this.creativeAsyncCall('handshakeVersion', version, cb)
+};
+
+VPAIDCreativeWrapper.prototype.initAd = function (width, height, viewMode, desiredBitrate, creativeData, environmentVars, cb) {
+  this._creative(width, height, viewMode, desiredBitrate, creativeData, environmentVars);
+  this.waitForEvent('AdLoaded', cb);
+};
+
+VPAIDCreativeWrapper.prototype.resizeAd = function (width, height, viewMode, cb) {
   this._creative.resizeAd(width, height, viewMode);
+  this.waitForEvent('AdSizeChange', cb);
 };
 
-VPAIDCreativeWrapper.prototype.pauseAd = function () {
+VPAIDCreativeWrapper.prototype.startAd = function (cb) {
+  this._creative.startAd();
+  this.waitForEvent('AdStarted', cb);
+};
+
+VPAIDCreativeWrapper.prototype.stopAd = function (cb) {
+  this._creative.stopAd();
+  this.waitForEvent('AdStopped', cb);
+};
+
+VPAIDCreativeWrapper.prototype.pauseAd = function (cb) {
   this._creative.pauseAd();
+  this.waitForEvent('AdPaused', cb);
 };
 
-VPAIDCreativeWrapper.prototype.resumeAd = function () {
+VPAIDCreativeWrapper.prototype.resumeAd = function (cb) {
   this._creative.resumeAd();
+  this.waitForEvent('AdPlaying', cb);
 };
 
-VPAIDCreativeWrapper.prototype.expandAd = function () {
+VPAIDCreativeWrapper.prototype.expandAd = function (cb) {
   this._creative.expandAd();
+  this.waitForEvent('AdExpandedChange', cb);
 };
 
-VPAIDCreativeWrapper.prototype.collapseAd = function () {
+VPAIDCreativeWrapper.prototype.collapseAd = function (cb) {
   this._creative.collapseAd();
+  this.waitForEvent('AdExpandedChange', cb);
 };
 
-//GETTERS and SETTERS
-VPAIDCreativeWrapper.prototype.getAdExpanded = function () {
-  console.log("getAdExpanded");
-  return this._creative.getAdExpanded();
+VPAIDCreativeWrapper.prototype.skipAd = function (cb) {
+  this._creative.skipAd();
+  this.waitForEvent('AdSkipped', cb);
+
+  //TODO: what about: AdSkippableStateChange
 };
 
-VPAIDCreativeWrapper.prototype.getAdSkippableState = function () {
-  console.log("getAdSkippableState");
-  return this._creative.getAdSkippableState();
-};
+//VPAID property getters
+[
+  'adLinear',
+  'adWidth',
+  'adHeight',
+  'adExpanded',
+  'adSkippableState',
+  'adRemainingTime',
+  'adDuration',
+  'adVolume',
+  'adCompanions',
+  'adIcons'
+].forEach(function (property) {
+  var getterName = 'get' + capitalize(property);
 
-VPAIDCreativeWrapper.prototype.getAdRemainingTime = function () {
-  console.log("getAdRemainingTime");
-  return this._creative.getAdRemainingTime();
-};
-
-VPAIDCreativeWrapper.prototype.getAdLinear = function () {
-  console.log("getAdLinear");
-  return this._creative.getAdLinear();
-};
-
-VPAIDCreativeWrapper.prototype.setAdVolume = function (val) {
-  this._creative.setAdVolume(val);
-};
-
-VPAIDCreativeWrapper.prototype.getAdVolume = function () {
-  return this._creative.getAdVolume();
-};
+  VPAIDCreativeWrapper.prototype[getterName] = function (cb) {
+    this.creativeAsyncCall(getterName, cb)
+  };
+});
--- test/ads/vpaid/VPAIDCreativeWrapper.spec.js
+++ test/ads/vpaid/VPAIDCreativeWrapper.spec.js
@@ -13,7 +13,8 @@ describe("VPAIDCreativeWrapper", function(){
       'expandAd': noop,
       'collapseAd': noop,
       'subscribe': noop,
-      'unsubscribe': noop
+      'unsubscribe': noop,
+      'unloadAdUnit': noop
     };
   });
 
@@ -51,6 +52,12 @@ describe("VPAIDCreativeWrapper", function(){
      });
     });
 
+    it("must complian if you pass an options that is not a hash", function(){
+      assert.throws(function() {
+        new VPAIDCreativeWrapper(vpaidCreative, 'foo');
+      }, VASTError, "on VPAIDCreativeWrapper, expected options hash  but got 'foo'")
+    });
+
     it("must publish the VPAIDCreative in '_creative'", function(){
       var wrapper = new VPAIDCreativeWrapper(vpaidCreative);
       assert.equal(wrapper._creative, vpaidCreative);
@@ -64,9 +71,236 @@ describe("VPAIDCreativeWrapper", function(){
       wrapper = new VPAIDCreativeWrapper(vpaidCreative);
     });
 
+    describe("creativeAsyncCall", function(){
+      beforeEach(function(){
+        this.clock = sinon.useFakeTimers();
+      });
+
+      afterEach(function(){
+        this.clock.restore();
+      });
+
+      it("must be a function", function(){
+        assert.isFunction(wrapper.creativeAsyncCall);
+      });
+
+      it("must complain if the method is not part of the creative", function(){
+        assert.throws(function () {
+          wrapper.creativeAsyncCall('foo', noop);
+        }, VASTError, "on VPAIDCreativeWrapper.creativeAsyncCall, invalid method name");
+      });
+
+      it("must complain if there is no callback", function(){
+        assert.throws(function () {
+          wrapper.creativeAsyncCall('initAd');
+        }, VASTError, "on VPAIDCreativeWrapper.creativeAsyncCall, missing callback");
+      });
+
+      it("must call the creative method", function(){
+        sinon.spy(vpaidCreative, 'initAd');
+        wrapper.creativeAsyncCall('initAd', noop);
+        sinon.assert.calledOnce(vpaidCreative.initAd)
+      });
+
+      it("must call the callback whenever the creative response comes back", function(){
+        var cb = sinon.spy();
+        sinon.spy(vpaidCreative, 'initAd');
+
+        wrapper.creativeAsyncCall('initAd', cb);
+        var wrapperCb = firstArg(vpaidCreative.initAd);
+        sinon.assert.notCalled(cb);
+        wrapperCb(null);
+
+        sinon.assert.calledOnce(cb);
+        sinon.assert.calledWith(cb, null);
+
+        //it must only call the callback once.
+        this.clock.tick(wrapper.options.responseTimeout);
+        sinon.assert.calledOnce(cb);
+      });
+
+      describe("on response timeout", function(){
+        it("must call the callback with a VASTError", function(){
+          var cb = sinon.spy();
+          sinon.spy(vpaidCreative, 'initAd');
+
+          wrapper.creativeAsyncCall('initAd', cb);
+          var wrapperCb = firstArg(vpaidCreative.initAd);
+          sinon.assert.notCalled(cb);
+          this.clock.tick(wrapper.options.responseTimeout);
+
+          sinon.assert.calledOnce(cb);
+          var error = firstArg(cb);
+          assert.instanceOf(error, VASTError);
+          assert.equal(error.message, "VAST Error: on VPAIDCreativeWrapper, timeout while waiting for a response on call 'initAd'");
+
+          //it must not call the callback again
+          wrapperCb(null);
+          sinon.assert.calledOnce(cb);
+        });
+
+        it("must destroy the creative", function(){
+          var cb = sinon.spy();
+          sinon.spy(wrapper, 'destroy');
+          wrapper.creativeAsyncCall('initAd', cb);
+          this.clock.tick(wrapper.options.responseTimeout);
+          sinon.assert.calledOnce(wrapper.destroy);
+        });
+
+      });
+    });
+
+    describe("destroy", function(){
+      it("must be a function", function(){
+        assert.isFunction(wrapper.destroy);
+      });
+
+      it("must call the creative's 'unloadAdUnit' method", function(){
+        sinon.stub(vpaidCreative, 'unloadAdUnit');
+        wrapper.destroy();
+        sinon.assert.calledOnce(vpaidCreative.unloadAdUnit);
+      });
+    });
+
     describe("on", function(){
       it("must be a function", function(){
-        assert.isFunction(wrapper.on)
+        assert.isFunction(wrapper.on);
+      });
+
+      it("must call the subscribe method of the inner creative", function(){
+        vpaidCreative.on = undefined;
+        vpaidCreative.addEventListener = undefined;
+        sinon.spy(vpaidCreative, 'subscribe');
+
+        wrapper.on('evtName', noop);
+        sinon.assert.calledWith(vpaidCreative.subscribe, 'evtName', noop);
+      });
+
+      it("must call the addEventListener method of the inner creative", function(){
+        vpaidCreative.subscribe = undefined;
+        vpaidCreative.on = undefined;
+        vpaidCreative.addEventListener = sinon.spy();
+
+        wrapper.on('evtName', noop);
+        sinon.assert.calledWith(vpaidCreative.addEventListener, 'evtName', noop);
+      });
+
+      it("must call the on method of the inner creative", function(){
+        vpaidCreative.subscribe = undefined;
+        vpaidCreative.addEventListener = undefined;
+        vpaidCreative.on = sinon.spy();
+
+        wrapper.on('evtName', noop);
+        sinon.assert.calledWith(vpaidCreative.on, 'evtName', noop);
+      });
+    });
+
+    describe("off", function(){
+      it("must be a function", function(){
+        assert.isFunction(wrapper.off);
+      });
+
+      it("must call the unsubscribe method of the inner creative", function(){
+        vpaidCreative.off = undefined;
+        vpaidCreative.removeEventListener = undefined;
+        sinon.spy(vpaidCreative, 'unsubscribe');
+
+        wrapper.off('evtName', noop);
+        sinon.assert.calledWith(vpaidCreative.unsubscribe, 'evtName', noop);
+      });
+
+      it("must call the removeEventListener method of the inner creative", function(){
+        vpaidCreative.unsubscribe = undefined;
+        vpaidCreative.on = undefined;
+        vpaidCreative.removeEventListener = sinon.spy();
+
+        wrapper.off('evtName', noop);
+        sinon.assert.calledWith(vpaidCreative.removeEventListener, 'evtName', noop);
+      });
+
+      it("must call the off method of the inner creative", function(){
+        vpaidCreative.unsubscribe = undefined;
+        vpaidCreative.removeEventListener = undefined;
+        vpaidCreative.off = sinon.spy();
+
+        wrapper.off('evtName', noop);
+        sinon.assert.calledWith(vpaidCreative.off, 'evtName', noop);
+      });
+    });
+
+    describe("waitForEvent", function(){
+      it("must be a function", function(){
+        assert.isFunction(wrapper.waitForEvent);
+      });
+
+      it("must complain if you don't pass an evtName", function(){
+        assert.throws(function () {
+          wrapper.waitForEvent();
+        }, VASTError, "on VPAIDCreativeWrapper.waitForEvent, missing evt name");
+      });
+      
+      it("must complain if you don't pass a callback", function(){
+        assert.throws(function () {
+          wrapper.waitForEvent('adInit');
+        }, VASTError, "on VPAIDCreativeWrapper.waitForEvent, missing callback");
+      });
+
+      it("must subscribe to the passed event", function(){
+        var listener = sinon.spy();
+        sinon.spy(wrapper, 'on');
+        wrapper.waitForEvent('adInit', listener);
+        sinon.assert.calledWith(wrapper.on, 'adInit', sinon.match.func);
+      });
+
+      describe("on response", function(){
+        it("must call the callback with the null as the first arg and the listener args afterwards", function(){
+          var callback = sinon.spy();
+          var listener;
+          sinon.spy(wrapper, 'on');
+          wrapper.waitForEvent('adInit', callback);
+          listener = secondArg(wrapper.on);
+
+          //We simulate the response
+          var evt = {type: 'adInit'};
+          listener(evt, 'foo', 'bar');
+
+          sinon.assert.calledWith(callback, null, evt, 'foo', 'bar');
+        });
+      });
+
+      describe("on response timeout", function(){
+        beforeEach(function(){
+          this.clock = sinon.useFakeTimers();
+        });
+
+        afterEach(function(){
+          this.clock.restore();
+        });
+
+        it("must call the callback with a vastError", function(){
+          var callback = sinon.spy();
+          var error;
+          sinon.spy(wrapper, 'on');
+          wrapper.waitForEvent('adInit', callback);
+          this.clock.tick(wrapper.options.responseTimeout);
+          sinon.assert.calledOnce(callback);
+          error = firstArg(callback);
+
+          assert.instanceOf(error, VASTError);
+          assert.equal(error.message, "VAST Error: on VPAIDCreativeWrapper.waitForEvent, timeout while waiting for event 'adInit'")
+        });
+
+        it("must not call the callback once the event response comes", function(){
+          var callback = sinon.spy();
+          var listener;
+          sinon.spy(wrapper, 'on');
+          wrapper.waitForEvent('adInit', callback);
+          listener = secondArg(wrapper.on);
+          this.clock.tick(wrapper.options.responseTimeout);
+          sinon.assert.calledOnce(callback);
+          listener('adInint');
+          sinon.assert.calledOnce(callback);
+        });
       });
     });
   });
