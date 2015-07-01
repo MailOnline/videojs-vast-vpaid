2ec40a8342df2792007caab13fa0df3d2ea1719e
Luis Silva <luis.dasilva@mailonline.co.uk>
2015-07-01 17:14:15 +0100

    [Luis] add more tests to loadAdUnit

--- src/ads/vpaid/VPAIDHTML5Tech.js
+++ src/ads/vpaid/VPAIDHTML5Tech.js
@@ -2,5 +2,34 @@ function VPAIDHTML5Tech(mediaFile) {
   if(!(this instanceof VPAIDHTML5Tech)) {
     return new VPAIDHTML5Tech(mediaFile);
   }
+  sanityCheck(mediaFile);
+  this.mediaFile = mediaFile;
+
+  function sanityCheck(mediaFile) {
+      if (!mediaFile || !isString(mediaFile.src)) {
+        throw new VASTError(VPAIDHTML5Tech.INVALID_MEDIA_FILE);
+      }
+  }
 }
+
+VPAIDHTML5Tech.prototype.loadAdUnit = function loadAdUnit(containerEl, callback) {
+  sanityCheck(containerEl, callback);
+  //TODO: will need a videoElement in this point
+
+
+  function sanityCheck(container, cb) {
+    if (!dom.isDomElement(container)) {
+      throw new VASTError(VPAIDHTML5Tech.INVALID_DOM_CONTAINER_EL);
+    }
+
+    if (!isFunction(cb)) {
+      throw new VASTError(VPAIDHTML5Tech.MISSING_CALLBACK);
+    }
+  }
+}
+
+var PREFIX = 'on VPAIDHTML5Tech';
+VPAIDHTML5Tech.INVALID_MEDIA_FILE = PREFIX + ', invalid MediaFile';
+VPAIDHTML5Tech.INVALID_DOM_CONTAINER_EL = PREFIX + ', invalid container HtmlElement';
+VPAIDHTML5Tech.MISSING_CALLBACK = PREFIX + ', missing valid callback';
 
--- test/ads/vpaid/VPAIDHTML5Tech.spec.js
+++ test/ads/vpaid/VPAIDHTML5Tech.spec.js
@@ -1,3 +1,5 @@
+var VAST_ERROR_PREFIX = 'VAST Error: ';
+
 describe("VPAIDHTML5Tech", function() {
   it("must be a function", function () {
     assert.isFunction(VPAIDHTML5Tech);
@@ -6,5 +8,45 @@ describe("VPAIDHTML5Tech", function() {
   it("must return an instance of itself", function() {
     assert.instanceOf(VPAIDHTML5Tech({src: 'fakeSource'}), VPAIDHTML5Tech);
   });
+
+  it("must complain if you don't pass a valid media file", function () {
+      [undefined, null, {}, []].forEach(function(invalidMediaFile) {
+          assert.throws(function () {
+              new VPAIDHTML5Tech(invalidMediaFile);
+          }, VASTError, VAST_ERROR_PREFIX + VPAIDHTML5Tech.INVALID_MEDIA_FILE);
+      });
+  });
+
+  describe("instance", function() {
+    var vpaidTech, testDiv;
+
+    beforeEach(function () {
+      vpaidTech = new VPAIDHTML5Tech({src: 'http://fake.mediaFile.url'});
+      testDiv = document.createElement("div");
+      document.body.appendChild(testDiv);
+    });
+
+    afterEach(function () {
+      dom.remove(testDiv);
+    });
+
+    describe("loadAdUnit", function() {
+      it("must throw a VASTError if you don't pass a valid dom Element to contain the ad", function() {
+          [undefined, null, {}, [], 123].forEach(function (invalidDOMElement) {
+            assert.throws(function (){
+              vpaidTech.loadAdUnit(invalidDOMElement);
+            }, VASTError, VAST_ERROR_PREFIX + VPAIDHTML5Tech.INVALID_DOM_CONTAINER_EL);
+          });
+      });
+
+      it("must throw a VASTError if you don't pass a callback to call once the ad have been loaded", function () {
+        [undefined, null, {}, 123].forEach(function(invalidCallback) {
+          assert.throws(function () {
+            vpaidTech.loadAdUnit(testDiv, invalidCallback);
+          }, VASTError, VAST_ERROR_PREFIX + VPAIDHTML5Tech.MISSING_CALLBACK);
+        });
+      })
+    });
+  })
 });
 
