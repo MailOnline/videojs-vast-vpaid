fec0d086cb7d1241af5682890516f94141b3de40
Luis Silva <luis.dasilva@mailonline.co.uk>
2015-08-05 14:52:09 +0100

    Update 2015-08-05T13:52:06.959Z

--- scripts/videojs-vast-vpaid.js
+++ scripts/videojs-vast-vpaid.js
@@ -1544,7 +1544,7 @@ function $addEventsSubscribers() {
 }
 
 function $clickThruHook(url, id, playerHandles) {
-    this._subscribers.trigger(AD_CLICK, {url: url, id: id, playerHandles: playerHandles});
+    this._subscribers.triggerSync(AD_CLICK, {url: url, id: id, playerHandles: playerHandles});
 }
 
 function $trigger(event) {
@@ -1802,6 +1802,12 @@ Subscriber.prototype.trigger = function(eventName, data) {
         setTimeout(function () {
             subscriber.handler.call(subscriber.context, data);
         }, 0);
+    });
+};
+
+Subscriber.prototype.triggerSync = function(eventName, data) {
+    this.get(eventName).forEach(function (subscriber) {
+        subscriber.handler.call(subscriber.context, data);
     });
 };
 
