9cab1c4e1a3b99198f89e8468f4fc16959b7620d
Carlos Serrano <carpasse@gmail.com>
2015-08-13 12:59:28 +0100

    Added precommit hook to run jsHint before every commit

---
+++ hooks/pre-commit.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+files=$(git diff --cached --name-only --diff-filter=ACM | grep ".js$")
+if [ "$files" = "" ]; then 
+    exit 0 
+fi
+
+pass=true
+
+echo "\nValidating JavaScript:\n"
+
+for file in ${files}; do
+    result=$(jshint ${file} | grep "${file} is OK")
+    if [ "$result" != "" ]; then
+        echo "\t\033[32mJSLint Passed: ${file}\033[0m"
+    else
+        echo "\t\033[31mJSLint Failed: ${file}\033[0m"
+        pass=false
+    fi
+done
+
+echo "\nJavaScript validation complete\n"
+
+if ! $pass; then
+    echo "\033[41mCOMMIT FAILED:\033[0m Your commit contains files that should pass JSHint but do not. Please fix the JSHint errors and try again.\n"
+    exit 1
+else
+    echo "\033[42mCOMMIT SUCCEEDED\033[0m\n"
+fi
