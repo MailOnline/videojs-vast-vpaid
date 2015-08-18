850e15ad81cca9b74931196cf3bd6d6f7124fe0c
carpasse <carpasse@gmail.com>
2015-08-18 13:09:12 +0100

    Worked on the release task

---
+++ build/bump.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var path = require('path');
+var config = require('./config');
+var bump = require('gulp-bump');
+var gutil = require('gulp-util');
+var git = require('gulp-git');
+var del = require('del');
+
+gulp.task('bump', function () {
+  var bumpType = config.bump || "patch";
+  return gulp.src(['./bower.json', './package.json'])
+    .pipe(bump({type: bumpType, version: config.options.version}).on('error', gutil.log))
+    .pipe(bump({type: bumpType}).on('error', gutil.log))
+    .pipe(gulp.dest('./'));
+});
