1655586d1d0e0b069a064068a759cc254e324bb8
TRAVIS_CI <carlos.pastor@mailonline.co.uk>
2015-08-28 10:04:40 +0000

    [RELEASE] build and release 0.1.7

--- demo/server/index.js
+++ demo/server/index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var app = express();
+var PORT = 8086;
 
 app.use(express.static(__dirname + '/../../dev'));
 
-app.listen(8085);
+app.listen(PORT);
 
-console.log("Server started Listening on port: 8085");
+console.log("Server started Listening on port: " + PORT);
 console.log("Serving static files from: ", __dirname + '/../dev');
 
 module.exports = app;
--- src/ads/videojs.vast.js
+++ src/ads/videojs.vast.js
@@ -221,8 +221,6 @@ vjs.plugin('vastClient', function VASTPlugin(options) {
     var adIntegrator = isVPAID(vastResponse) ? new VPAIDIntegrator(player, settings) : new VASTIntegrator(player);
     var adFinished = false;
 
-    player.vast.adUnit = adIntegrator.playAd(vastResponse, callback);
-
     playerUtils.once(player, ['vast.adStart', 'vast.adsCancel'], function (evt) {
       if (evt.type === 'vast.adStart') {
         addAdsLabel();
@@ -234,6 +232,8 @@ vjs.plugin('vastClient', function VASTPlugin(options) {
     if (isIDevice()) {
       preventManualProgress();
     }
+
+    player.vast.adUnit = adIntegrator.playAd(vastResponse, callback);
 
     /*** Local functions ****/
     function addAdsLabel() {
@@ -255,7 +255,7 @@ vjs.plugin('vastClient', function VASTPlugin(options) {
       var skipad_attempts = 0;
 
       player.on('timeupdate', adTimeupdateHandler);
-      playerUtils.once(player, ['vast.adEnd', 'vast.adsCancel'], stopPreventManualProgress);
+      playerUtils.once(player, ['vast.adEnd', 'vast.adsCancel', 'vast.adError'], stopPreventManualProgress);
 
       /*** Local functions ***/
       function adTimeupdateHandler() {
