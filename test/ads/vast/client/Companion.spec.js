a4d4afb81f0747ccdcee8c4ad138924f3ba98676
alex-phillips <ahp118@gmail.com>
2016-01-15 14:03:30 -0500

    simplified creativeXML test

--- test/ads/vast/client/Companion.spec.js
+++ test/ads/vast/client/Companion.spec.js
@@ -14,18 +14,16 @@ describe("Companion", function(){
     });
   });
 
+  var companionXML = '<Companion width="300" height="250">' +
+        '<StaticResource creativeType="image/jpeg"><![CDATA[http://www.example.com/image.jpg]]></StaticResource>' +
+        '<TrackingEvents><Tracking event="creativeView"><![CDATA[http://www.example.com/whatever]]></Tracking></TrackingEvents>' +
+        '<CompanionClickThrough><![CDATA[http://www.example.com/tracker.html]]></CompanionClickThrough>' +
+    '</Companion>';
+
   //Optional Elements
   describe("trackingEvents", function(){
     /*jshint maxlen: 500 */
     it("must be filled with all the tracking events that the xml provides", function(){
-      var companionXML = '<Companion>' +
-        '<StaticResource creativeType="image/jpeg"><![CDATA[http://www.example.com/image.jpg]]></StaticResource>'+
-        '<TrackingEvents>'+
-        '<Tracking event="creativeView">'+
-        '<![CDATA[http://t4.liverail.com/?metric=view25&pos=0&coid=135&pid=1331&nid=1331&oid=229&olid=2291331&cid=331&tpcid=&vid=&amid=&cc=default&pp=&vi=0&vv=&sg=&tsg=&pmu=0&pau=0&psz=0&ctx=&tctx=&coty=7&adt=0&did=&buid=&scen=&mca=&mma=&mct=0&url=http%3A%2F%2Fwww.iab.net%2Fguidelines%2F508676%2Fdigitalvideo%2Fvsuite%2Fvast%2Fvast_copy%2Fvast_xml_samples&trid=54afb53ccfddf1.12174006&bidf=0.10000&bids=0.00000&bidt=1&bidh=0&bidlaf=0&sdk=0&cb=8279.195.234.241.9.0&ver=1&w=&wy=&x=&y=&xy=]]>'+
-        '</Tracking>'+
-        '</TrackingEvents>'+
-        '</Companion>';
       var companion = Companion(xml.toJXONTree(companionXML));
       var trackingEvents = companion.trackingEvents;
 
@@ -38,14 +36,6 @@ describe("Companion", function(){
 
   describe("dimensions", function(){
     it("must contain creative dimensions", function(){
-      var companionXML = '<Companion width="300" height="250">' +
-        '<StaticResource creativeType="image/jpeg"><![CDATA[http://www.example.com/image.jpg]]></StaticResource>'+
-        '<TrackingEvents>'+
-        '<Tracking event="creativeView">'+
-        '<![CDATA[http://t4.liverail.com/?metric=view25&pos=0&coid=135&pid=1331&nid=1331&oid=229&olid=2291331&cid=331&tpcid=&vid=&amid=&cc=default&pp=&vi=0&vv=&sg=&tsg=&pmu=0&pau=0&psz=0&ctx=&tctx=&coty=7&adt=0&did=&buid=&scen=&mca=&mma=&mct=0&url=http%3A%2F%2Fwww.iab.net%2Fguidelines%2F508676%2Fdigitalvideo%2Fvsuite%2Fvast%2Fvast_copy%2Fvast_xml_samples&trid=54afb53ccfddf1.12174006&bidf=0.10000&bids=0.00000&bidt=1&bidh=0&bidlaf=0&sdk=0&cb=8279.195.234.241.9.0&ver=1&w=&wy=&x=&y=&xy=]]>'+
-        '</Tracking>'+
-        '</TrackingEvents>'+
-        '</Companion>';
       var companion = Companion(xml.toJXONTree(companionXML));
       assert.equal(companion.width, 300);
       assert.equal(companion.height, 250);
