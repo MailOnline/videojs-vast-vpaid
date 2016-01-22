0e9882641e0734a1032fe5b1c2ca4629767277c6
alex-phillips <ahp118@gmail.com>
2016-01-22 10:09:58 -0500

    added companion ad support for videojs-5x branch

--- src/scripts/ads/vast/Creative.js
+++ src/scripts/ads/vast/Creative.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var Linear = require('./Linear');
+var Companion = require('./Companion');
+var utilities = require('../../utils/utilityFunctions');
 
 function Creative(creativeJTree) {
   if(!(this instanceof Creative)) {
@@ -14,6 +16,17 @@ function Creative(creativeJTree) {
 
   if(creativeJTree.linear) {
     this.linear = new Linear(creativeJTree.linear);
+  }
+
+  if (creativeJTree.companionAds) {
+    var companions = [];
+    var companionAds = creativeJTree.companionAds && creativeJTree.companionAds.companion;
+    companionAds = utilities.isArray(companionAds) ? companionAds : [companionAds];
+    companionAds.forEach(function (companionData) {
+      companions.push(new Companion(companionData));
+    });
+
+    this.companionAds = companions;
   }
 }
 
