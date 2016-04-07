be06dadcf954c988874383ac55381ebb9147939e
Quentin Pré <pre.quentin@gmail.com>
2016-04-07 17:48:26 +0200

    [FIX] typo

--- src/scripts/utils/mimetypes.js
+++ src/scripts/utils/mimetypes.js
@@ -1,25 +1,24 @@
-var HTML5MimeTypes = [
-  'text/javascript',
-  'text/javascript1.0',
-  'text/javascript1.2',
-  'text/javascript1.4',
-  'text/jscript',
-  'application/javascript',
-  'application/x-javascript',
-  'text/ecmascript',
-  'text/ecmascript1.0',
-  'text/ecmascript1.2',
-  'text/ecmascript1.4',
-  'text/livescript',
-  'application/ecmascript',
-  'application/x-ecmascript',
-];
+'use strict';
 
-var FlashMIMETypes = [
-  'application/x-shockwave-flash'
-];
+module.exports = {
+  html5: [
+    'text/javascript',
+    'text/javascript1.0',
+    'text/javascript1.2',
+    'text/javascript1.4',
+    'text/jscript',
+    'application/javascript',
+    'application/x-javascript',
+    'text/ecmascript',
+    'text/ecmascript1.0',
+    'text/ecmascript1.2',
+    'text/ecmascript1.4',
+    'text/livescript',
+    'application/ecmascript',
+    'application/x-ecmascript',
+  ],
 
-module.export = {
-  html5: HTML5MimeTypes,
-  flash: FlashMIMETypes,
+  flash: [
+    'application/x-shockwave-flash'
+  ],
 };
