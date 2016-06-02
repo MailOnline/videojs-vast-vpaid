322cfcbb65bf5cf8ca5d8662a00d2965507c8035
mol-automation <fabioroger+mol@gmail.com>
2016-06-02 14:33:29 +0000

    [RELEASE] build and release 2.0.0

---
+++ src/scripts/utils/mimetypes.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  html5: [
+    'text/javascript',
+    'text/javascript1.0',
+    'text/javascript1.2',
+    'text/javascript1.4',
+    'text/jscript',
+    'application/javascript',
+    'application/x-javascript',
+    'text/ecmascript',
+    'text/ecmascript1.0',
+    'text/ecmascript1.2',
+    'text/ecmascript1.4',
+    'text/livescript',
+    'application/ecmascript',
+    'application/x-ecmascript',
+  ],
+
+  flash: [
+    'application/x-shockwave-flash'
+  ],
+};
