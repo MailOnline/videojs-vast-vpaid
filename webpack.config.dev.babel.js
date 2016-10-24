c6fad8013e142980219ce1e9b7398f8f51138d18
Carlos <carpasse@gmail.com>
2016-10-24 16:52:09 +0100

    chore: add dev/prod builds
    create webpack prod/dev builds and link them to npm scripts

--- webpack.config.dev.babel.js
+++ webpack.config.dev.babel.js
@@ -1,14 +1,13 @@
 import { join } from 'path';
-import { main } from './package.json';
+import { main, name } from './package.json';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default {
-  entry: {
-    "videojs-vast-vpaid.umd": main
-  },
+  entry: main,
   output: {
     path: join(__dirname, 'dist'),
     libraryTarget: 'umd',
-    filename: '[name].js'
+    filename: `${name}.umd.js`
   },
   devtool: 'cheap-module-eval-source-map',
   module: {
@@ -16,10 +15,48 @@ export default {
       {
         test: /\.js$/,
         loader: 'babel',
-
-        // Exclude all modules but the VPAID module dependencies
-        exclude: /node_modules\/(?!vpaid).+/
+        include:[
+          join(__dirname, 'src'),
+          join(__dirname, 'node_modules/vpaid-flash-client/js'),
+          join(__dirname, 'node_modules/vpaid-html5-client/js')
+        ],
+        query: {
+          presets: ['es2015']
+        }
+      },
+      {
+        test: /\.scss$/,
+        loaders: ExtractTextPlugin.extract({
+          loader: [
+            {
+              loader: 'css',
+              query:  {
+                modules: true,
+                importLoaders: 1,
+                localIdentName: '[name]__[local]___[hash:base64:5]',
+                minimize: false
+              }
+            },
+            {
+              loader: 'autoprefixer-loader',
+              query: {
+                browsers: [
+                  '> 1%',
+                  'last 3 versions',
+                  'iOS > 6',
+                  'ie > 9'
+                ],
+                cascade: false
+              }
+            },
+            'resolve-url',
+            'sass'
+          ]
+        })
       }
     ]
-  }
-}
+  },
+  plugins: [
+    new ExtractTextPlugin(`${name}.css`)
+  ]
+};
--- webpack.config.prod.babel.js
+++ webpack.config.prod.babel.js
@@ -1,15 +1,14 @@
 import { join } from 'path';
-import { main } from './package.json';
+import { main, name } from './package.json';
 import webpack from 'webpack';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default {
-  entry: {
-    "videojs-vast-vpaid.umd.min": main
-  },
+  entry: main,
   output: {
     path: join(__dirname, 'dist'),
     libraryTarget: 'umd',
-    filename: '[name].js'
+    filename: `${name}.umd.min.js`
   },
   devtool: 'source-map',
   module: {
@@ -17,9 +16,49 @@ export default {
       {
         test: /\.js$/,
         loader: 'babel',
-
-        // Exclude all modules but the VPAID module dependencies
-        exclude: /node_modules\/(?!vpaid).+/
+        include:[
+          join(__dirname, 'src'),
+          join(__dirname, 'node_modules/vpaid-flash-client/js'),
+          join(__dirname, 'node_modules/vpaid-html5-client/js')
+        ],
+        query: {
+          presets: ['es2015']
+        }
+      },
+      {
+        test: /\.scss$/,
+        loaders: ExtractTextPlugin.extract({
+          loader: [
+            {
+              loader: 'css',
+              query:  {
+                modules: true,
+                importLoaders: 1,
+                localIdentName: '[name]__[local]___[hash:base64:5]',
+                minimize: true,
+                autoprefixer: false,
+                zindex: false,
+                mergeIdents: false,
+                reduceIdents: false,
+                discardUnused: false
+              }
+            },
+            {
+              loader: 'autoprefixer-loader',
+              query: {
+                browsers: [
+                  '> 1%',
+                  'last 3 versions',
+                  'iOS > 6',
+                  'ie > 9'
+                ],
+                cascade: false
+              }
+            },
+            'resolve-url',
+            'sass'
+          ]
+        })
       }
     ]
   },
@@ -32,7 +71,8 @@ export default {
         screw_ie8: true,
         warnings: false
       }
-    })
+    }),
+    new ExtractTextPlugin(`${name}.min.css`)
   ]
 
 }
